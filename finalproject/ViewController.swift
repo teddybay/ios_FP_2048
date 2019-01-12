5e9dc187d75361aafa88bf3dd72d1f28d6c92a95
“teddybay” <fishbone647243@gmail.com>
2019-01-12 14:10:16 +0800

    Initial Commit

---
+++ finalproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  finalproject
+//
+//  Created by 陳伯墉 on 2019/1/12.
+//  Copyright © 2019年 00457006. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
